8b358ab26136a12f62f5db2908a6eb445c609b09
William Courtney <william.michael.courtney@gmail.com>
2015-03-08 11:56:27 -0700

    Reverted coop to ES5 and added event emitters on change.

--- models/coop.js
+++ models/coop.js
@@ -1,21 +1,23 @@
-let five = require('johnny-five'),
+var events = require('events'),
+    five = require('johnny-five'),
     Edison = require("edison-io"),
     board = new five.Board({
         io: new Edison()
     });
 
-let doorOpen = Symbol('doorOpen');
-let temperatureC = Symbol('temperatureC');
-let batteryVoltage = Symbol('batteryVoltage');
-
 function initTemperature(self) {
     // temperature sensor  on A0
-    let temperature = new five.Temperature({
+    var temperature = new five.Temperature({
         controller: "GROVE",
-        pin: "A0"
+        pin: "A0",
+        freq: 10000
     });
     temperature.on("change", function(err, data) {
-        self[temperatureC] = Math.round(temperature.celcius);
+        var newVal = Math.round(temperature.celsius);
+        if (self.temperatureC != newVal) {
+            self.temperatureC = newVal;
+            self.emit('change', ['temperatureC']);
+        }
     });
 }
 function initBattery(self) {
@@ -27,44 +29,57 @@ function initBattery(self) {
     });
 
     battery.scale(0,100).on('change', function() {
-        self[batteryVoltage] = this.value;
+        self.batteryVoltage = this.value;
+        self.emit('change', ['batteryVoltage']);
     });
 }
 function initDoor(self) {
     // door sensor on D2
-    let doorSensor = new five.Button(2);
+    var doorSensor = new five.Button(2);
     doorSensor.on("press", function() {
-        self[doorOpen] = false;
+        console.log('got a press');
+        self.doorOpen = 0;
+        self.emit('change', ['doorOpen']);
     });
     doorSensor.on("release", function() {
-        self[doorOpen] = true;
+        console.log('got a release');
+        self.doorOpen = 1;
+        self.emit('change', ['doorOpen']);
     });
 }
-class Coop {
-    constructor() {
-        // set defaults
-        this[doorOpen] = false;
-        this[temperatureC] = 0;
-        this[batteryVoltage] = 99;
-
-        board.on('ready', () => {
-          initTemperature(this);
-          initBattery(this);
-          initDoor(this);
-
-        });
-    }
-    toString() {
-        var self = this;
-        return JSON.stringify({
-            doorOpen: self[doorOpen],
-            temperatureC: self[temperatureC],
-            batteryVoltage: self[batteryVoltage],
-        });
-    }
-    get doorOpen() { return this[doorOpen]; }
-    get temperatureC() { return this[temperatureC]; }
-    get batteryVoltage() { return this[batteryVoltage]; }
+function initMotor(self) {
+    // servo motor on D3
+    self.motor = new five.Servo({
+        pin: 3,
+        startAt: 10
+    });
 }
+module.exports = function() {
+    var coop = new events.EventEmitter()
 
-module.exports = Coop;
+    // set defaults
+    coop.doorOpen = 0;
+    coop.temperatureC = 0;
+    coop.batteryVoltage = 99;
+
+    board.on('ready', function() {
+        initTemperature(coop);
+        initBattery(coop);
+        //initMotor(coop);
+        initDoor(coop);
+    });
+    coop.toString = function () {
+        return JSON.stringify({
+            doorOpen: coop.doorOpen,
+            temperatureC: coop.temperatureC,
+            batteryVoltage: coop.batteryVoltage,
+        });
+    };
+    coop.openDoor = function () {
+        coop.motor.to(10);
+    }
+    coop.closeDoor = function () {
+        coop.motor.to(100);
+    }
+    return coop;
+}
