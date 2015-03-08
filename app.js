d29b819992db74d3cc0ce5ebd846e265ae211367
William Courtney <william.michael.courtney@gmail.com>
2015-03-08 13:43:51 -0700

    Reorganized variable declaration and added heartbeat to app.

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var Coop = require('./models/coop');
-
-var coopReporter = require('./lib/coopReporter');
-var smsReporter = require('./lib/smsReporter');
-
-// build coop
-var myCoop = new Coop();
+var Coop = require('./models/coop'),
+    coopReporter = require('./lib/coopReporter'),
+    smsReporter = require('./lib/smsReporter'),
+    socket = require('socket.io-client')('http://localhost:3000'),
+    myCoop = new Coop();
 
 // set up reporter
 coopReporter(myCoop);
-//smsReporter(myCoop);
+smsReporter(myCoop);
 
 // log the coop state every second for testing
 setInterval(function() {
     console.log('toString',myCoop.toString());
 }, 1000);
+
+// send heartbeat to log
+setInterval(function() {
+    console.log('heartbeat');
+    myCoop.trigger('heartbeat');
+}, 1000 * 5); // todo: send heartbeat every 10 minutes
+
+socket.on('doorOpen', function() {
+    myCoop.openDoor();
+});
+socket.on('doorClose', function() {
+    myCoop.closeDoor();
+});
