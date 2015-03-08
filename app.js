c4e8f9c849ede66a604751a2748e84381856eecb
William Courtney <william.michael.courtney@gmail.com>
2015-03-08 11:56:46 -0700

    Added m2x connectivity.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-require('babel/register');
 var Coop = require('./models/coop');
 
+var coopReporter = require('./lib/coopReporter');
+var smsReporter = require('./lib/smsReporter');
+
+// build coop
 var myCoop = new Coop();
+
+// set up reporter
+coopReporter(myCoop);
+//smsReporter(myCoop);
 
 // log the coop state every second for testing
 setInterval(function() {
